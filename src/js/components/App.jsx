c9f4da150232322eabad07487c6a0e67cc3ebba3
Daniel Mcveigh <DanielMcveigh@tfl.gov.uk>
2019-03-06 15:06:44 +0000

    Moved stacking order of elements

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -4,15 +4,15 @@ import Form from "./Form";
 
 const App = () => (
     <div className="row mt-5">
-      <div className="col-md-4 offset-md-1">
-        <h2>Players</h2>
-        <PlayerList />
-      </div>
-      <div className="col-md-4 offset-md-1">
-        <h2>Add a new player</h2>
-        <Form />
-      </div>
+        <div className="col-md-4 offset-md-1">
+            <h2>Add a new player</h2>
+            <Form />
+        </div>
+        <div className="col-md-4 offset-md-1">
+            <h2>Players</h2>
+            <PlayerList />
+        </div>
     </div>
-  );
+);
 
 export default App;
