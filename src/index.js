72d908848a154a51caa6bae49533d2b4dfbd1502
Daniel Mcveigh <DanielMcveigh@tfl.gov.uk>
2019-03-06 12:51:47 +0000

    Allowed store to be modified from the browser console.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import { Provider } from "react-redux";
 import store from "./js/store/index";
 import App from './js/components/App';
+// Allows the state to be manipulated from the console
+import index from "./js/index";
 
 render(
     <Provider store={store}>
