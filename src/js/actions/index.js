c088e937962e66544673f2ad1a60223ab79e50b6
Daniel Mcveigh <DanielMcveigh@tfl.gov.uk>
2019-03-08 14:02:44 +0000

    Added sorting action

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,5 +1,5 @@
 // Import actionTypes from ../constants
-import { ADD_PLAYER, REMOVE_PLAYER } from "../constants/actionTypes";
+import { ADD_PLAYER, REMOVE_PLAYER, SORT_PLAYERS } from "../constants/actionTypes";
 
 export function addPlayer(payload) {
     return {
@@ -11,6 +11,13 @@ export function addPlayer(payload) {
 export function removePlayer(payload) {
     return {
         type: REMOVE_PLAYER,
+        payload
+    }
+};
+
+export function sortPlayers(payload) {
+    return {
+        type: SORT_PLAYERS,
         payload
     }
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 import store from "../js/store/index";
 // Import actions here
-import { addPlayer, removePlayer } from "./actions/index";
+import { addPlayer, removePlayer, sortPlayers } from "./actions/index";
 
 window.store = store;
 // Connect actions here
 // window.ACTIONNAME = ACTIONNAME
 window.addPlayer = addPlayer;
 window.removePlayer = removePlayer;
+window.sortPlayers = sortPlayers;
